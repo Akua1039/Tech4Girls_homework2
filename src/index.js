c84bc1e43372debf16ce3204f59ecba26c6fc891
Akua1039 <fosua27@gmail.com>
2024-11-25 17:17:04 +0000

    added index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let name = prompt("What is your name?");
+let age = prompt("How old are you?"); 
+let email =prompt("What is your email address");
+
+ alert(`Hi ${name}, you are ${age} years old and we will keep in touch via your email: ${email}.👋🏽👋🏽`)
